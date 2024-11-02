45f69b06569ea4c6e9b51d98e599c6d66668be2f
Azran Hossain <azranhossain@outlook.com>
2024-11-02 21:09:23 +0600

    added models and db-setup

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
 
-from api import courses, section, user
+from api import courses, sections, users
+from db.db_setup import engine
+from db.models import course, user
+
+user.Base.metadata.create_all(bind=engine)
+course.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-app.include_router(user.router)
-app.include_router(section.router)
+app.include_router(users.router)
+app.include_router(sections.router)
 app.include_router(courses.router)
